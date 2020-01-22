2c3f0ec164d6efc90e4de5d041d6e4827a0df363
purnima1201 <52439197+purnima1201@users.noreply.github.com>
2020-01-22 19:19:46 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -18,32 +18,31 @@ function draw() {
   movingRect.x = World.mouseX;
   movingRect.y = World.mouseY;
 
-  /*if(isTouching(movingRect,fixedRect)){
-    movingRect.shapeColor = "red";
+   if(isTouching(movingRect,obj)){
+    //.shapeColor = "red";
     fixedRect.shapeColor = "red";
-  }
-  else{
-    movingRect.shapeColor = "green";
+   }
+   else{
+    //movingRect.shapeColor = "green";
     fixedRect.shapeColor = "green";
-  } */ 
-  
-  if(isTouching(movingRect,obj)){
+   }
+   if(isTouching(movingRect,fixedRect)){
     movingRect.shapeColor = "red";
     obj.shapeColor = "red";
-  }
-  else{
+   }
+   else{
     movingRect.shapeColor = "green";
     obj.shapeColor = "green";
-  }
+   }
   
   drawSprites();
 }
 
 function isTouching(a,b){
   if (a.x - b.x < b.width/2 + a.width/2
-      && b.x - a.x < b.width/2 + a.width/2
-      && a.y - b.y < b.height/2 + a.height/2
-      && b.y - a.y < b.height/2 + a.height/2) {
+    && b.x - a.x < b.width/2 + a.width/2
+    && a.y - b.y < b.height/2 + a.height/2
+    && b.y - a.y < b.height/2 + a.height/2) {
     return true;
   }
   else {
